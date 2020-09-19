8df34e03ab6076f9b34afb87a7d088b7af609b0d
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-19 18:21:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,9 @@ numbers = [1, 2, 3]
 counter = 0
 
 def square_array(numbers)
-  new_numbers = []
+  
   while numbers[counter] do
-   new_numbers << numbers**
+  numbers**
    counter += 1
   end
 end
