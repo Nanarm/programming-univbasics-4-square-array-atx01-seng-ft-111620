671a2c60df5823a368fe0c7450b93747e772e055
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-19 18:06:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ numbers = [1,2,3]
 def square_array(numbers)
   new_numbers = 
   while numbers.length <= 3 do
-    new_numbers << numbers.each**2
+    new_numbers << numbers.each
     numbers.shift
   end
 end
